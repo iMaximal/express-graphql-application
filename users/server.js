290fd45443fb65531c3fc7610d4c4e4a7e143740
Maximal <maxim@infofree.ru>
2018-02-13 00:41:37 +0500

    add graphql in express

--- users/server.js
+++ users/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
 
 const app = express()
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true // todo only in development
+}))
 
 app.listen(4000, () => {
   console.log('Listening')
